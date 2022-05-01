e7effae7c114defb3bbdfdffdeb5fa803f5e4299
GJT <Dell@DESKTOP-MRJEHML.GJT>
2022-05-01 11:23:31 +0800

    add tittle of src/a.cpp

--- src/a.cpp
+++ src/a.cpp
@@ -1,3 +1,11 @@
+/*
+ * GJT @ Dell
+ * 2022.5.1
+ * 
+ * @ Frist use Git and Github
+ * @ For text
+ *
+ */
 #include<stdio>
 void main(){
 printf(This program witmesses my frist useing of Git and Github.);
